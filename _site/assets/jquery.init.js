a79acf7c70962fb5f33d6a6ab4b55dc9f8c7d7bb
metamn <bartus.csongor@gmail.com>
2013-09-04 16:26:25 +0300

    Frontpage done. Scrolling between slides done

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 
+  // Scrolling between divs through navigation
+  $('#navigation li').click(function() {
+    var index = $(this).index() + 2;
+    
+    var slide = $('section:nth-child(' + index + ')');
+    scrollTo(slide);
+    
+    $('#navigation li').removeClass('active');
+    $(this).addClass('active');
+    
+    return false;
+  });
+  
+
+  // Scrolling to a div
+  function scrollTo(div) {
+    $("html, body").animate({ scrollTop: $(div).offset().top }, "slow");
+    return false;
+  }
 });
