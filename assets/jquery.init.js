0f4bf7e05801a4be03e536a4b887a94b7b63ea56
metamn <bartus.csongor@gmail.com>
2013-09-05 10:37:12 +0300

    Navigation done

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -2,24 +2,49 @@ $(document).ready(function() {
 
   // Navigation: scrolling to the section
   $('#navigation li').click(function() {
-    var index = $(this).index() + 2;
-    
+    if ($(this).hasClass('active')) {
+      menuShow();
+    } else {
+      menuScroll($(this));
+    }
+  });
+  
+  
+  // Scroll to a section on clicking a menu item
+  function menuScroll(item) {
+    var index = item.index() + 2;
+      
     var slide = $('section:nth-child(' + index + ')');
     scrollTo(slide);
     
     $('#navigation li').removeClass('active');
-    $(this).addClass('active');
-    
-    menu();
-    
-    return false;
-  });
+    item.addClass('active');
+    menuHide();
+  }
   
   
-  // Menu: active item is visible; the others hidden
-  // - it behaves like a menu on mobiles and smartphones
-  function menu() {
+  // Shows the otherwise hidden menu elements
+  function menuShow() {
+    $('#navigation li').show('slow');
+  }
+  
+  
+  // Make the active item is visible; the others hidden
+  // The menu is pulled left to let the content be fully visible
+  function menuHide() {
+    // Move out to left
+    $('#navigation').animate( {
+      left: '1.25em'
+    }, 'slow', function() {
+      // Animation complete.
+    });
     
+    // Hide non-active items
+    $('#navigation li').each(function() {
+      if (!($(this).hasClass('active'))) {
+        $(this).hide('slow');
+      }
+    });
   }
   
 
